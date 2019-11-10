edce43e75aca3e15de80e11448b3d49881d42d52
Xueying Li <lixue@seas.upenn.edu>
2019-11-10 15:15:34 -0800

    test class and json jar added

---
+++ src/WeatherCollectionTest.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WeatherCollectionTest {
+
+	@Test
+	void testWeatherCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReadCityURL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCityKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReadForecastURL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWeatherParser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetWeatherList() {
+		fail("Not yet implemented");
+	}
+
+}

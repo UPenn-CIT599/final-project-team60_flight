cce537edb0b5008fd2123b8d85a369beb1469b7d
Xueying Li <lixue@seas.upenn.edu>
2019-11-12 11:32:36 -0800

    add test for CityReader

---
+++ src/CityReaderTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CityReaderTest {
+	
+	CityReader cr;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		cr = new CityReader();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetAllCities() {
+		ArrayList<City> cities = cr.getAllCities();
+		City hk = cities.get(0);
+		assertEquals(1, hk.getRank());
+		assertEquals("Hong Kong", hk.getName());
+	}
+
+}
